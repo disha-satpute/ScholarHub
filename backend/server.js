34f8d9af9959157bcc9944a5575e2018e2772274
Vaishnavi <dishasatpute44@gmail.com>
2026-05-27 21:07:01 +0530

    server.js changed

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ require('dotenv').config();
 const db = require('./config/db');
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: "https://scholar-hub-xer4.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/scholarships', scholarshipRoutes);
